38af0193dc8a2e66bde0acb311ba8e591e46d73c
Chase Franklin <cmf8781@utulsa.edu>
2020-11-15 13:51:42 -0600

    update default input

--- data/example.pas
+++ data/example.pas
@@ -1,22 +1,11 @@
 program example(input, output);
-var x : integer; var y: integer; var z: integer;
-var r : real; var s : real;
-function gcd(a, b: integer ): integer;
+var x : integer; var y :  integer;
+function gcd(a: integer ; b : integer ): integer;
 begin
    if b = 0 then gcd := a
    else gcd := gcd(b, a mod b)
 end;
-
 begin
-   read(x, y);
-   write(gcd(x, y))
-end.
-
-begin
-   z := 104;
-   x := 2;
-   z := 21234;
-   
-   r := 3.14;
-   s := 3.14E2;
+   z := read(x, y);
+   z := write(gcd(x, y));
 end.
