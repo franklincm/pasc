4479d00349cbc50f103a15ed7ae7e915d1f755d4
Chase Franklin <cmf8781@utulsa.edu>
2020-11-19 13:59:36 -0600

    synerr testing

--- data/example.pas
+++ data/example.pas
@@ -1,11 +1,13 @@
 program example(input, output);
-var x : integer; var y :  integer;
+var x :integer
+var y :integer;
+var z :integer;
 function gcd(a: integer ; b : integer ): integer;
 begin
    if b = 0 then gcd := a
    else gcd := gcd(b, a mod b)
 end;
 begin
-   z := read(x, y);
-   z := write(gcd(x, y));
+   input := read(x, y);
+   output := write(gcd(x, y));
 end.
